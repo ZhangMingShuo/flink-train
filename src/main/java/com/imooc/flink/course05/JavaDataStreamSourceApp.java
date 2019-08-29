88602b3e2c04243459d552c38d4b59b4c5e53476
ZhangMingShuo <1170709161@qq.com>
2019-08-29 14:17:23 +0800

    split Select in java

--- src/main/java/com/imooc/flink/course05/JavaDataStreamSourceApp.java
+++ src/main/java/com/imooc/flink/course05/JavaDataStreamSourceApp.java
@@ -1,15 +1,37 @@
 package com.imooc.flink.course05;
 
+import org.apache.flink.streaming.api.collector.selector.OutputSelector;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SplitStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class JavaDataStreamSourceApp {
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         //nonParallelSourceFunction(env);
         //parallelSourceFunction(env);
-        richParallelSourceFunction(env);
+        //richParallelSourceFunction(env);
+        splitSelectFunction(env);
         env.execute("JavaDataStreamSourceApp");
+    }
+
+    private static void splitSelectFunction(StreamExecutionEnvironment env) {
+        DataStreamSource<Long> data = env.addSource(new JavaCustomNonParallelSourceFunction());
+        SplitStream<Long> splits = data.split(new OutputSelector<Long>() {
+            @Override
+            public Iterable<String> select(Long value) {
+                List<String> output = new ArrayList<>();
+                if (value % 2 == 0)
+                    output.add("even");
+                else
+                    output.add("odd");
+                return output;
+            }
+        });
+        splits.select("odd").print().setParallelism(1);
     }
 
     private static void nonParallelSourceFunction(StreamExecutionEnvironment env) {
@@ -35,4 +57,5 @@ public class JavaDataStreamSourceApp {
                 .setParallelism(2);
         data.print();
     }
+
 }

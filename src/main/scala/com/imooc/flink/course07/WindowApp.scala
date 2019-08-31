46ec0664d4c1bc36d732f65fd8f240724568c6ef
ZhangMingShuo <1170709161@qq.com>
2019-08-31 16:23:59 +0800

    Window App count words in scala

---
+++ src/main/scala/com/imooc/flink/course07/WindowApp.scala
@@ -0,0 +1,19 @@
+package com.imooc.flink.course07
+
+import org.apache.flink.streaming.api.scala._
+import org.apache.flink.streaming.api.windowing.time.Time
+
+object WindowApp {
+  def main(args: Array[String]): Unit = {
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+    val text = env.socketTextStream("localhost",9999)
+    text.flatMap(_.split(","))
+      .map((_,1))
+      .keyBy(0)
+      .timeWindow(Time.seconds(5))
+      .sum(1)
+      .print()
+      .setParallelism(1)
+    env.execute("WindowApp")
+  }
+}
